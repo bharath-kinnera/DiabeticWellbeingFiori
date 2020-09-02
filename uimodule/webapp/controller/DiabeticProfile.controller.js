4df1c8edc8782788143a9da2c26ec0c8c6e94e8c
bharath-kinnera <bharath.kinnera@sap.com>
2020-09-02 11:22:12 +0000

    UI Change

--- uimodule/webapp/controller/DiabeticProfile.controller.js
+++ uimodule/webapp/controller/DiabeticProfile.controller.js
@@ -1,6 +1,7 @@
 sap.ui.define([
-  "T1D/DiabeticWellbeing/controller/BaseController"
-], function(Controller) {
+  "T1D/DiabeticWellbeing/controller/BaseController",
+  "sap/ui/model/json/JSONModel"
+], function(Controller,JSONModel) {
   "use strict";
 
   return Controller.extend("T1D.DiabeticWellbeing.controller.DiabeticProfile", {
@@ -12,6 +13,24 @@ sap.ui.define([
     onClick: function(){
         var oRouter = this.getOwnerComponent().getRouter();
         oRouter.navTo("GlucoseLevel");
+    },
+
+    onEditPress: function(){
+      this.getView().byId("idSimpleForm").setVisible(true);
+    },
+
+    OnSelectionChange: function(oEvent){
+      var json = new JSONModel();
+      var selectedData = oEvent.getSource().getSelectedItem().getBindingContext().getObject();
+      json.setData(selectedData);
+      this.getView().setModel(json,"DiabeticProfile");
+      debugger;
+    },
+
+    onSavePress: function(oEvent){
+      var formData = this.getView().getModel("DiabeticProfile").getData();
+      //this.getOwnerComponent().getModel().
+      this.getView().byId("idSimpleForm").setVisible(false);
     }
 
   });
