a87194f478543f08dd87346db07f6da5d4707cc7
bharath-kinnera <bharath.kinnera@sap.com>
2020-09-02 15:39:37 +0000

    UI Changes

--- uimodule/webapp/controller/DiabeticProfile.controller.js
+++ uimodule/webapp/controller/DiabeticProfile.controller.js
@@ -8,6 +8,9 @@ sap.ui.define([
 
     onInit: function(){
       this.getView().addStyleClass("sapUISizeCompact");
+      this.getView().byId("idProfEdit").setEnabled(false);
+      this.getView().byId("idProfSave").setEnabled(false);
+      this.getView().byId("idProfReset").setEnabled(false);
     },
 
     onClick: function(){
@@ -16,7 +19,11 @@ sap.ui.define([
     },
 
     onEditPress: function(){
+      this.getView().byId("idDiabeticProfTable").setVisible(false);
       this.getView().byId("idSimpleForm").setVisible(true);
+      this.getView().byId("idProfEdit").setEnabled(false);
+      this.getView().byId("idProfSave").setEnabled(true);
+      this.getView().byId("idProfReset").setEnabled(true);
     },
 
     OnSelectionChange: function(oEvent){
@@ -24,13 +31,43 @@ sap.ui.define([
       var selectedData = oEvent.getSource().getSelectedItem().getBindingContext().getObject();
       json.setData(selectedData);
       this.getView().setModel(json,"DiabeticProfile");
-      debugger;
-    },
+      this.getView().byId("idProfEdit").setEnabled(true);
+
+     },
 
     onSavePress: function(oEvent){
       var formData = this.getView().getModel("DiabeticProfile").getData();
       //this.getOwnerComponent().getModel().
+/*
+        var fnSuccess = function () {
+				this._setBusy(false);
+				MessageToast.show(this._getText("changesSentMessage"));
+				this._setUIChanges(false);
+			}.bind(this);
+
+			var fnError = function (oError) {
+				this._setBusy(false);
+				this._setUIChanges(false);
+				MessageBox.error(oError.message);
+			}.bind(this);
+
+			this._setBusy(true); // Lock UI until submitBatch is resolved.
+			this.getView().getModel().submitBatch("peopleGroup").then(fnSuccess, fnError);
+			this._bTechnicalErrors = false; // If there were technical errors, a new save resets them.
+*/
+      this.getView().byId("idDiabeticProfTable").setVisible(true);
       this.getView().byId("idSimpleForm").setVisible(false);
+      this.getView().byId("idProfEdit").setEnabled(false);
+      this.getView().byId("idProfSave").setEnabled(false);
+      this.getView().byId("idProfReset").setEnabled(false);
+      this.getView().byId("idDiabeticProfTable").removeSelections(true);
+    },
+
+    onResetPress: function(oEvent){
+      var json = new JSONModel();
+      var selectedData = oEvent.getSource().getSelectedItem().getBindingContext().getObject();
+      json.setData(selectedData);
+      this.getView().byId("idDiabeticProfTable").removeSelections(true);
     }
 
   });
--- uimodule/webapp/controller/GlucoseLevelDetails.controller.js
+++ uimodule/webapp/controller/GlucoseLevelDetails.controller.js
@@ -1,5 +1,7 @@
 sap.ui.define([
   "T1D/DiabeticWellbeing/controller/BaseController"
+  //"sap.ui.core.util.Export",
+  //"sap.ui.core.util.ExportTypeCSV"
 ], function(Controller) {
   "use strict";
 
@@ -7,12 +9,46 @@ sap.ui.define([
 
     onInit: function(){
       this.getView().addStyleClass("sapUISizeCompact");
+
     },
 
     onClick1: function(){
         var oRouter1 = this.getOwnerComponent().getRouter();
         oRouter1.navTo("RouteApp");
+    },
+/////
+
+    onClickExport: sap.m.Table.prototype.exportData || function(oEvent) {
+ /*     var oExport = new sap.ui.core.util.Export({
+      exportType: new sap.ui.core.util.ExportTypeCSV({
+      separatorChar: ";"
+      }),
+
+      models: this.getView().getModel(),
+      rows: {
+      path: "/GlucoseLevels"
+      },
+
+      columns: [
+        {
+          name: "ID",
+          template: {
+          content: "{GlucoseLevels>/ID}"
+          }
+        }
+      ]
+
+    });
+
+    //* download exported file
+
+    oExport.saveFile().always(function() {
+    this.destroy();
+    }); */
     }
 
+
+
+/////
   });
 });
